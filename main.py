37516bc23a28ef52090c69c4331306558e039d0a
Chris Mac <cmac@rrc.ca>
2025-10-02 09:01:36 -0500

    implement average score calculation

--- main.py
+++ main.py
@@ -11,12 +11,32 @@ Enter score for Cara: 88
 Enter player name (or "done"): done
 """
 # store players as a list of tuples
-players = [("Sally", 95), ("Toby", 88), ("Sandeep", 10), ("Zainab", 5)]
+players = [("Sally", 95), ("Toby", 90), ("Sandeep", 10), ("Zainab", 5)]
 
 # setup variables to store summary values
 highest_score_player = (None, 0)
 lowest_score_player = (None, 0)
 average_score = 0
+
+
+## pull the scores into a separate list
+# declare a new variable to hold a list of scores
+player_scores = []
+score_total = 0
+
+# iterate over the players list
+# add each score to the new list
+for player in players:
+    player_scores.append(player[1])
+    score_total += player[1]
+
+# print(score_total)
+# print(player_scores)
+# find the average
+# add all the scores together
+# divide by the number of scores
+# set the value of average_score
+average_score = score_total / len(player_scores)
 
 
 """
@@ -34,4 +54,4 @@ print("=== Summary ===")
 print(f"Players: {len(players)}")
 print(f"Highest: {None}") # Sally - 95
 print(f"Lowest:  {None}") # Zainab - 5
-print(f"Average: {None}") 
+print(f"Average: {average_score}") 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+print(score_total)
+# print(player_scores)
