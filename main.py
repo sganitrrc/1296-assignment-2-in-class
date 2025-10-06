ebb54e682fa21c02d7e335d2ab3620a615fc0e29
sganitrrc <sganitrrc@rrc.ca>
2025-10-06 18:36:19 -0500

    Create lowest player score function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from score_stats import calculate_avg_score
 from output import output_player_summary
 from input import input_players_and_score
 from output import get_highest_score
+from output import get_lowest_score
 """
 input example:
 
@@ -21,11 +22,12 @@ Enter player name (or "done"): done
 players = [("Sally", 95), ("Toby", 90), ("Sandeep", 10), ("Zainab", 5)]
 
 highest_player = ()
+lowest_player = ()
 
 # setup variables to store summary values
 num_players = len(players)
 highest_score_player = get_highest_score(players, highest_player)
-lowest_score_player = (None, 0)
+lowest_score_player = get_lowest_score(players, lowest_player)
 average_score = calculate_avg_score(players)
 
 # Output player summary
--- output.py
+++ output.py
@@ -14,5 +14,17 @@ def get_highest_score(players, highest_player):
         if player[1] > highest_player_score:
             highest_player_score = player[1]
             highest_player = player
-            
+
     return highest_player
+
+def get_lowest_score(players, lowest_player):
+
+    lowest_player_score = players[0][1]
+    
+    for player in players:
+
+        if player[1] < lowest_player_score:
+            lowest_player_score = player[1]
+            lowest_player = player
+
+    return lowest_player
