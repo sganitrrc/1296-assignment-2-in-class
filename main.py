4ea87d53cb27be212d5c6d36c542ccecb12939be
sganitrrc <sganitrrc@rrc.ca>
2025-10-06 18:42:09 -0500

    allow user input / comment set players list

--- main.py
+++ main.py
@@ -3,23 +3,12 @@ from output import output_player_summary
 from input import input_players_and_score
 from output import get_highest_score
 from output import get_lowest_score
-"""
-input example:
 
-Enter player name (or "done"): Alice
-Enter score for Alice: 95
-Enter player name (or "done"): Bob
-Enter score for Bob: notanumber
-Invalid score. Skipping this entry.
-Enter player name (or "done"): Cara
-Enter score for Cara: 88
-Enter player name (or "done"): done
-"""
 # store players as a list of tuples
-#players = []
-#input_players_and_score(players)
-#print(players)
-players = [("Sally", 95), ("Toby", 90), ("Sandeep", 10), ("Zainab", 5)]
+players = []
+input_players_and_score(players)
+
+# players = [("Sally", 95), ("Toby", 90), ("Sandeep", 10), ("Zainab", 5)]
 
 highest_player = ()
 lowest_player = ()
@@ -32,4 +21,3 @@ average_score = calculate_avg_score(players)
 
 # Output player summary
 output_player_summary(num_players, highest_score_player, lowest_score_player, average_score)
-# some random change
